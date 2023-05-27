fdc51d96920a7ef04ae859bd391e724c2f63d3ee
Shery <chsheharyarahmed@gmail.com>
2023-05-27 14:23:46 +0500

    initialized and scaffolded

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Icons } from "components/icons"
+
+import { cn } from "@/lib/utils"
+
+export default function Navbar() {
+  return (
+    <div>
+      <Icons.siteLogo className={cn("")} />
+    </div>
+  )
+}
