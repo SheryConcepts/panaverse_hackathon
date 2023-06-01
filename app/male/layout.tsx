4346c5d1f9d4487a8b9cfa5cbacf78bc0aaf43ec
Shery <chsheharyarahmed@gmail.com>
2023-06-01 14:38:35 +0500

    FEATURE: added male page

--- app/male/layout.tsx
+++ app/male/layout.tsx
@@ -1,6 +1,5 @@
 
-export default function Layout() {
-    return (
-    <p>hello</p>
+export default function Layout({children}: {children: React.ReactNode}) { return (
+    <div>{children}</div>
     )
   }
--- app/male/page.tsx
+++ app/male/page.tsx
@@ -1,3 +1,41 @@
-export default function() {
-    return <div>hello</div>
-  }
+import Link from "next/link";
+import { groqFetch } from "@/sanity/lib/client";
+import ProductCard from "@/components/product-card";
+
+export default async function Page() {
+  
+  const products = await groqFetch(`
+    *[_type == "product" && "male" in productCategory] {
+        _id,
+        productTitle,
+        productType,
+        productPrice,
+        productCategory,
+        productSizes,
+        "productImages": productImages[].asset->url,
+        productSlug,
+        "productDetails": productInformation.productDetails,
+        "productCare": productInformation.productCare
+}`)
+
+
+
+  return (
+    <div className="flex flex-wrap justify-around items-center gap-y-4">
+      {products.map((p: any) => (
+        <Link
+          href={`/products/${p.productSlug.current}`}
+          key={p._id}
+        >
+          <ProductCard
+            productImage={p.productImages[0] ?? ""}
+            productTitle={p.productTitle}
+            productType={p.productType}
+            productPrice={p.productPrice}
+            key={p._id}
+          />
+        </Link>
+      ))}
+    </div>
+  )
+}
